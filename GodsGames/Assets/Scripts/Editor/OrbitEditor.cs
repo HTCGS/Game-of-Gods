268879a1d15e5ae452c434f37d24719b40794edd
Kirill Suhanov <kirjadoc@inbox.lv>
2018-04-05 11:33:18 +0300

    orbitEditor

--- GodsGames/Assets/Scripts/Editor/OrbitEditor.cs
+++ GodsGames/Assets/Scripts/Editor/OrbitEditor.cs
@@ -32,6 +32,8 @@ public class OrbitEditor : Editor
     {
         Orbit orbit = target as Orbit;
 
+        if (orbit.Eccentricity < 0 || orbit.Eccentricity >= 1) return;
+
         float focus = orbit.Eccentricity;
         Vector3 toParent = orbit.Parent.transform.position - orbit.transform.position;
         float a = toParent.magnitude / (1 - focus);
@@ -40,15 +42,15 @@ public class OrbitEditor : Editor
         Vector3 center = orbit.transform.position + (toParent.normalized * a);
         Vector3 direction = orbit.transform.position + (toParent.normalized * (2 * a));
 
-        Vector3 bSide = orbit.GetSpaceVelocityVector(toParent, orbit.OrbitDirection);
-        Vector3 leftB = orbit.transform.position + (bSide.normalized * b);
-        Vector3 rightB = orbit.transform.position - (bSide.normalized * b);
+        Vector3 bSide = orbit.GetSpaceVelocityVector(toParent, orbit.OrbitDirection).normalized;
+        Vector3 leftB = orbit.transform.position + (bSide * b);
+        Vector3 rightB = orbit.transform.position - (bSide * b);
 
-        Vector3 leftDirB = direction + (bSide.normalized * b);
-        Vector3 rightDirB = direction - (bSide.normalized * b);
+        Vector3 leftDirB = direction + (bSide * b);
+        Vector3 rightDirB = direction - (bSide * b);
 
-        Vector3 centerLeft = center + (bSide.normalized * b);
-        Vector3 centerRight = center - (bSide.normalized * b);
+        Vector3 centerLeft = center + (bSide * b);
+        Vector3 centerRight = center - (bSide * b);
 
         Handles.DrawBezier(orbit.transform.position, centerLeft, leftB, centerLeft, Color.white, null, 2);
         Handles.DrawBezier(centerLeft, direction, leftDirB, direction, Color.white, null, 2);
@@ -58,15 +60,23 @@ public class OrbitEditor : Editor
 
         //Scale
 
+        float capSize = toParent.magnitude;
+        if (capSize > 3) capSize /= 2;
+
         EditorGUI.BeginChangeCheck();
         Handles.color = Color.red;
         float scaleLeftB = Handles.ScaleValueHandle(orbit.Eccentricity, centerLeft,
-            Quaternion.LookRotation(centerLeft - center), 1, Handles.ArrowHandleCap, 0.01f);
+            Quaternion.LookRotation(centerLeft - center), capSize, Handles.CubeHandleCap, 0.01f);
         scaleLeftB -= 0.5f;
 
         if (EditorGUI.EndChangeCheck())
         {
-            Undo.RecordObject(target, "B scale");
+            float angle = Vector3.Angle(-Vector3.forward, centerLeft - center);
+            if (angle < 45) scaleLeftB *= -1;
+            angle = Vector3.Angle(-Vector3.right, centerLeft - center);
+            if (angle < 45) scaleLeftB *= -1;
+
+            Undo.RecordObject(target, "Left B scale");
             float delta = scaleLeftB - lastScale;
             lastScale = scaleLeftB;
 
@@ -83,12 +93,17 @@ public class OrbitEditor : Editor
         EditorGUI.BeginChangeCheck();
         Handles.color = Color.red;
         float scaleRightB = Handles.ScaleValueHandle(orbit.Eccentricity, centerRight,
-            Quaternion.LookRotation(centerRight - center), 1, Handles.ArrowHandleCap, 0.01f);
+            Quaternion.LookRotation(centerRight - center), capSize, Handles.CubeHandleCap, 0.01f);
         scaleRightB -= 0.5f;
 
         if (EditorGUI.EndChangeCheck())
         {
-            Undo.RecordObject(target, "B scale");
+            float angle = Vector3.Angle(Vector3.forward, centerRight - center);
+            if (angle < 45) scaleRightB *= -1;
+            angle = Vector3.Angle(Vector3.right, centerRight - center);
+            if (angle < 45) scaleRightB *= -1;
+
+            Undo.RecordObject(target, "Right B scale");
             float delta = scaleRightB - lastScale;
             lastScale = scaleRightB;
 
@@ -105,16 +120,21 @@ public class OrbitEditor : Editor
         EditorGUI.BeginChangeCheck();
         Handles.color = Color.red;
         float scaleA = Handles.ScaleValueHandle(orbit.Eccentricity, direction,
-            Quaternion.LookRotation(direction - center), 1, Handles.ArrowHandleCap, 0.01f);
+            Quaternion.LookRotation(direction - center), capSize, Handles.CubeHandleCap, 0.01f);
         scaleA -= 0.5f;
 
         if (EditorGUI.EndChangeCheck())
         {
+            float angle = Vector3.Angle(Vector3.forward, direction - center);
+            if (angle < 45) scaleA *= -1;
+            angle = Vector3.Angle(Vector3.right, direction - center);
+            if (angle < 45) scaleA *= -1;
+
             Undo.RecordObject(target, "A scale");
             float delta = scaleA - lastScale;
             lastScale = scaleA;
 
-            if ((a + Mathf.Sign(-delta) * 0.01f) > b)
+            if ((a + Mathf.Sign(-delta) * 0.01f) >= b)
             {
                 a += Mathf.Sign(-delta) * 0.01f;
                 float c = Mathf.Sqrt((a * a) - (b * b));
